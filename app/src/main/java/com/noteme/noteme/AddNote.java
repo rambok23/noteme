3f01389d2708806a712a4d2a878310951f899539
rambok23 <katkarrohit59@gmail.com>
2021-06-30 18:49:06 +0530

    initial commit

---
+++ app/src/main/java/com/noteme/noteme/AddNote.java
@@ -0,0 +1,143 @@
+package com.noteme.noteme;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class AddNote extends AppCompatActivity {
+    Toolbar toolbar;
+    EditText noteTitle,noteDetails;
+    ImageView voice;
+    Calendar c;
+    String todaysDate;
+    String currentTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("New Note");
+
+        noteDetails = findViewById(R.id.noteDetails);
+        noteTitle = findViewById(R.id.noteTitle);
+        voice = findViewById(R.id.voice);
+
+        noteTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() != 0){
+                    getSupportActionBar().setTitle(s);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        // set current date and time
+        c = Calendar.getInstance();
+        todaysDate = c.get(Calendar.YEAR)+"/"+(c.get(Calendar.MONTH)+1)+"/"+c.get(Calendar.DAY_OF_MONTH);
+        Log.d("DATE", "Date: "+todaysDate);
+        currentTime = pad(c.get(Calendar.HOUR))+":"+pad(c.get(Calendar.MINUTE));
+        Log.d("TIME", "Time: "+currentTime);
+
+    }
+
+    private String pad(int time) {
+        if(time < 10)
+            return "0"+time;
+        return String.valueOf(time);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.save){
+            if(noteTitle.getText().length() != 0){
+                Note note = new Note(noteTitle.getText().toString(),noteDetails.getText().toString(),todaysDate,currentTime);
+                SimpleDatabase sDB = new SimpleDatabase(this);
+                long id = sDB.addNote(note);
+                Note check = sDB.getNote(id);
+                Log.d("inserted", "Note: "+ id + " -> Title:" + check.getTitle()+" Date: "+ check.getDate());
+                onBackPressed();
+
+                Toast.makeText(this, "Note Saved.", Toast.LENGTH_SHORT).show();
+            }else {
+                noteTitle.setError("Title Can not be Blank.");
+            }
+
+        }else if(item.getItemId() == R.id.delete){
+            Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent ,10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                assert result != null;
+                noteDetails.setText(result.get(0));
+            }
+        }
+
+    }
+}
---
+++ app/src/main/java/com/noteme/noteme/Edit.java
@@ -0,0 +1,146 @@
+package com.noteme.noteme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Edit extends AppCompatActivity {
+    Toolbar toolbar;
+    EditText nTitle,nContent;
+    Calendar c;
+    String todaysDate;
+    String currentTime;
+    long nId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        Intent i = getIntent();
+        nId = i.getLongExtra("ID",0);
+        SimpleDatabase db = new SimpleDatabase(this);
+        Note note = db.getNote(nId);
+
+        final String title = note.getTitle();
+        String content = note.getContent();
+        nTitle = findViewById(R.id.noteTitle);
+        nContent = findViewById(R.id.noteDetails);
+        nTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                getSupportActionBar().setTitle(title);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.length() != 0){
+                    getSupportActionBar().setTitle(s);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        nTitle.setText(title);
+        nContent.setText(content);
+
+        // set current date and time
+        c = Calendar.getInstance();
+        todaysDate = c.get(Calendar.YEAR)+"/"+(c.get(Calendar.MONTH)+1)+"/"+c.get(Calendar.DAY_OF_MONTH);
+        Log.d("DATE", "Date: "+todaysDate);
+        currentTime = pad(c.get(Calendar.HOUR))+":"+pad(c.get(Calendar.MINUTE));
+        Log.d("TIME", "Time: "+currentTime);
+    }
+
+
+    private String pad(int time) {
+        if(time < 10)
+            return "0"+time;
+        return String.valueOf(time);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.save){
+            Note note = new Note(nId,nTitle.getText().toString(),nContent.getText().toString(),todaysDate,currentTime);
+            Log.d("EDITED", "edited: before saving id -> " + note.getId());
+            SimpleDatabase sDB = new SimpleDatabase(getApplicationContext());
+            long id = sDB.editNote(note);
+            Log.d("EDITED", "EDIT: id " + id);
+            goToMain();
+            Toast.makeText(this, "Note Edited.", Toast.LENGTH_SHORT).show();
+        }else if(item.getItemId() == R.id.delete){
+            Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void goToMain() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent ,10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                assert result != null;
+                nContent.setText(result.get(0));
+            }
+        }
+
+    }
+
+
+}
